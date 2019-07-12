8b93f206b856f81f7a0669123d40073f8e05dd6d
Dima Duc <dimaduc@gmail.com>
2019-07-12 10:06:43 +0700

    sua menu truyen trong may tinh

--- src/Truyen.js
+++ src/Truyen.js
@@ -123,12 +123,15 @@ xemTrangKhac = (e,{value}) => {
           hienVaGiauPhoneMenu } = this.props;
     return (
       <div className="Truyen">
+
+
         {docx_or_pdf
         ? <div className='truyen-div' 
             style={{fontSize: kichCuocChuDocx+'pt', lineHeight: kichCuocChuDocx+5+'pt', 
               backgroundColor: (white_or_black ? 'black' : 'white'), 
               color: (white_or_black ? 'white' : 'black'),
               width: (chieuRongManHinh-30), 
+
             }}>
             {/*Hien thi truyen DOCX, xem tat ca trang*/}
             
@@ -147,9 +150,15 @@ xemTrangKhac = (e,{value}) => {
           <p>Page {trang} of {tongSoTrang}</p>
         </div>
         }
-        <MenuDieuKhien white_or_black={white_or_black} dangXemGi={dangXemGi} chieuRongManHinh={chieuRongManHinh} 
+ 
+        {docx_or_pdf && (chieuRongManHinh > 900)
+          ? null
+          : <MenuDieuKhien white_or_black={white_or_black} dangXemGi={dangXemGi} chieuRongManHinh={chieuRongManHinh} 
                        docx_or_pdf={docx_or_pdf} arrayTatCa={arrayTatCaTrang} xemTruoc={this.xemTrangTruoc}
                        xemSau={this.xemTrangSau} xemKhac={this.xemTrangKhac} hienVaGiauPhoneMenu={hienVaGiauPhoneMenu}/>
+        }
+
+        
         </div>
     )
   }
