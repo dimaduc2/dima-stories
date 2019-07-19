2f1116c39b470caff251c168d63f8756c63958c0
Dima Duc <dimaduc@gmail.com>
2019-07-19 12:02:47 +0700

    Updates

---
+++ precache-manifest.6596ea75e4f8e092a5eeb1f8cf04a5b4.js
@@ -0,0 +1,346 @@
+self.__precacheManifest = [
+  {
+    "revision": "c41b59317c8fdd2b6ee77c20ab59dd60",
+    "url": "/dima-stories/static/media/birthday__ponner89_nh_halo.c41b5931.jpg"
+  },
+  {
+    "revision": "2c18df5671d6ba703303",
+    "url": "/dima-stories/static/css/main.ed15fc61.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/dima-stories/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "a71b1a6a6e2bd3cc75fc",
+    "url": "/dima-stories/static/js/1.a71b1a6a.chunk.js"
+  },
+  {
+    "revision": "26071fd92bec7b1a906a",
+    "url": "/dima-stories/static/js/runtime~main.26071fd9.js"
+  },
+  {
+    "revision": "c4a34dfe87d871a3b3c4cc438c8eaef2",
+    "url": "/dima-stories/static/media/Gray_Jedi_(Jedi_&_Sith)_symbol.c4a34dfe.png"
+  },
+  {
+    "revision": "64b041f2d913369dd0bb9c424fb6e4a6",
+    "url": "/dima-stories/static/media/overlord_symbol.64b041f2.png"
+  },
+  {
+    "revision": "57282a1759ea2a1a21fbcab0c0b4af5f",
+    "url": "/dima-stories/static/media/Gray_Jedi_Revan.57282a17.jpg"
+  },
+  {
+    "revision": "2305da2176297fa3f443e65213fd5097",
+    "url": "/dima-stories/static/media/Anakin_and_Padme_Jedi_s_Star_Wars_revenge_of_the_Sith.2305da21.jpg"
+  },
+  {
+    "revision": "958cc12f35462bc4798fe5b2f0eb999e",
+    "url": "/dima-stories/static/media/Obi-Wan_vs_Siri_(Padawan).958cc12f.png"
+  },
+  {
+    "revision": "970d8af67d796c9312791c5806f6f131",
+    "url": "/dima-stories/static/media/Recovery_(Darth_Vader_vs_Luke_vs_Leia).970d8af6.png"
+  },
+  {
+    "revision": "ac5e24808897b9b758712d873a254c5f",
+    "url": "/dima-stories/static/media/skywalkers_s_family.ac5e2480.jpg"
+  },
+  {
+    "revision": "3de9df2c37e92301378217f0fe2c70df",
+    "url": "/dima-stories/static/media/Batman_Arkham_Knight_Demon.3de9df2c.jpg"
+  },
+  {
+    "revision": "9ab8dbf35154fc46af34b94e9c29c158",
+    "url": "/dima-stories/static/media/Azrael_as_Batman.9ab8dbf3.jpg"
+  },
+  {
+    "revision": "fb90fbb81548c9cc45827e7aa164a624",
+    "url": "/dima-stories/static/media/Batman_Family.fb90fbb8.jpg"
+  },
+  {
+    "revision": "3909068316736ee3a587c870814fb754",
+    "url": "/dima-stories/static/media/batman_of_the_ages.39090683.jpg"
+  },
+  {
+    "revision": "2e113bc868867db731b15a8de4e300e0",
+    "url": "/dima-stories/static/media/Batman_vs_Superman_vs_Wonder_Women.2e113bc8.jpg"
+  },
+  {
+    "revision": "e8f3496deb4f9379dccf109b0b8f659e",
+    "url": "/dima-stories/static/media/Iron_Spider-Man_suit_by_nanotechnology.e8f3496d.jpg"
+  },
+  {
+    "revision": "c8c83d773402b602f37393a4ab242992",
+    "url": "/dima-stories/static/media/Spider-Man Homecoming suit + Vibranium suit for Black Panther.c8c83d77.jpg"
+  },
+  {
+    "revision": "bf1f7de4ed753728e5d0a07427f88635",
+    "url": "/dima-stories/static/media/Scream_love_Spider_man.bf1f7de4.jpg"
+  },
+  {
+    "revision": "9f2c7dec80ed001e4e512578113f3587",
+    "url": "/dima-stories/static/media/Spiderman_Ultimate_Symbiote_(Spider_Man_have Venom_and_) Carnage.9f2c7dec.jpg"
+  },
+  {
+    "revision": "7a8a60236d2918e87645e6ce8b2543a7",
+    "url": "/dima-stories/static/media/venom__peter_parker__vs_toxin__eddie_brock.7a8a6023.jpg"
+  },
+  {
+    "revision": "e0b1bb9b8d133e4e0c3be67cb6c6523d",
+    "url": "/dima-stories/static/media/gantz_collab_(kurono_help_tae).e0b1bb9b.jpg"
+  },
+  {
+    "revision": "91fe3802b9b3116255f1c1edcbedbe3b",
+    "url": "/dima-stories/static/media/kuronotwo_y_reika.91fe3802.jpg"
+  },
+  {
+    "revision": "8187868f60fdee324f030e2a79d18616",
+    "url": "/dima-stories/static/media/KuronoXTae.8187868f.jpg"
+  },
+  {
+    "revision": "6cee9d9fa3729fdde2a5ad604f516ae5",
+    "url": "/dima-stories/static/media/Gantz_-_Kei_[Version_1].6cee9d9f.png"
+  },
+  {
+    "revision": "f56dbecac160fbd599e2e6bcb20a3b49",
+    "url": "/dima-stories/static/media/gantz__oka_super_suitw.f56dbeca.jpg"
+  },
+  {
+    "revision": "61650a2d4115bc3869208546fc0cc063",
+    "url": "/dima-stories/static/media/Ash_s_harem.61650a2d.png"
+  },
+  {
+    "revision": "619026ec8bc8fe38e137bc02928a44be",
+    "url": "/dima-stories/static/media/What_if_Ash_became_evil_in_Pokemon_Hoopa_movie.619026ec.jpg"
+  },
+  {
+    "revision": "901825754c091fdb785167a8a563f758",
+    "url": "/dima-stories/static/media/Recovershipping_-_AshGardevoir.90182575.jpg"
+  },
+  {
+    "revision": "7d59fb8bc6591e7610c45ab9957f0fa4",
+    "url": "/dima-stories/static/media/aaml_goes_team_rocket.7d59fb8b.jpg"
+  },
+  {
+    "revision": "e826f0ba670601334ace6589704e63ef",
+    "url": "/dima-stories/static/media/ash_aura_awake.e826f0ba.jpg"
+  },
+  {
+    "revision": "540c8ed7e2160f2bdff5ab3d3414b0c2",
+    "url": "/dima-stories/static/media/Angelic_Being.540c8ed7.jpg"
+  },
+  {
+    "revision": "62fb839cdc516623681dda778827eb28",
+    "url": "/dima-stories/static/media/Evangelion_Unit_01_crazy_red_2.62fb839c.jpg"
+  },
+  {
+    "revision": "bf691be0b954ccb3b3a0f7db13fdb84b",
+    "url": "/dima-stories/static/media/EVAxP3.bf691be0.jpg"
+  },
+  {
+    "revision": "75e72d927aee0004b9b419f358638945",
+    "url": "/dima-stories/static/media/Lilith_Crazy_Red.75e72d92.jpg"
+  },
+  {
+    "revision": "f06f4d563d489c652eaae11990546fc3",
+    "url": "/dima-stories/static/media/Redemption.f06f4d56.jpg"
+  },
+  {
+    "revision": "2353d49d73a8a544171f50260fd8edba",
+    "url": "/dima-stories/static/media/Grid_Protector_and_Enforcer.2353d49d.jpg"
+  },
+  {
+    "revision": "fb2bcca067a70325b6881f2b09ed2ebb",
+    "url": "/dima-stories/static/media/The_Cut_Short_Cafe.fb2bcca0.jpg"
+  },
+  {
+    "revision": "b36cab7ce8c477a889dacc896ae99a02",
+    "url": "/dima-stories/static/media/Tron_-_System_Restarting.b36cab7c.png"
+  },
+  {
+    "revision": "5304eaae058eb40984068cdf7e752644",
+    "url": "/dima-stories/static/media/TRON_Revolt.5304eaae.jpg"
+  },
+  {
+    "revision": "3ed6b7c5e8b44dc8b8d3e9b74eae9d49",
+    "url": "/dima-stories/static/media/Tron_Rinzler.3ed6b7c5.jpg"
+  },
+  {
+    "revision": "97a7f2c0bc61915d61d5a1744047a293",
+    "url": "/dima-stories/static/media/2000_hits.97a7f2c0.jpg"
+  },
+  {
+    "revision": "2c18df5671d6ba703303",
+    "url": "/dima-stories/static/js/main.2c18df56.chunk.js"
+  },
+  {
+    "revision": "ed19eff95443e119f06037a104df45ab",
+    "url": "/dima-stories/static/media/Hunter_Nin.ed19eff9.jpg"
+  },
+  {
+    "revision": "616e632a1d22b38400107ba5ee0df73d",
+    "url": "/dima-stories/static/media/naruto___the_true_sage_of_six_paths__fanart.616e632a.jpg"
+  },
+  {
+    "revision": "6bfa35d7c38d85468197a014c5a67e27",
+    "url": "/dima-stories/static/media/naruto_eyes__body_and_chakra_of_the_juubi.6bfa35d7.jpg"
+  },
+  {
+    "revision": "21de0b757e109c765a7024c8c68466f4",
+    "url": "/dima-stories/static/media/koTruyen.21de0b75.docx"
+  },
+  {
+    "revision": "0bc18abe8663e27013e7463ccc56b3b3",
+    "url": "/dima-stories/static/media/koTruyen.0bc18abe.pdf"
+  },
+  {
+    "revision": "44f438b1adcddbe31fba479086db88f8",
+    "url": "/dima-stories/static/media/Back to the future.44f438b1.docx"
+  },
+  {
+    "revision": "c9e2cad01e7af30558799695d1c1b9d2",
+    "url": "/dima-stories/static/media/Back to the future.c9e2cad0.pdf"
+  },
+  {
+    "revision": "cee9650a383e1bea8e8e37c5aad12a68",
+    "url": "/dima-stories/static/media/Changing the Future.cee9650a.docx"
+  },
+  {
+    "revision": "52ac48ef588d380c7386c54c39b755f2",
+    "url": "/dima-stories/static/media/Changing the Future.52ac48ef.pdf"
+  },
+  {
+    "revision": "e93c00f2b9136bcacd3fb0907a974d27",
+    "url": "/dima-stories/static/media/evangelion.e93c00f2.docx"
+  },
+  {
+    "revision": "51c8e8ca0117f9d3786cc1d6f9a80f4e",
+    "url": "/dima-stories/static/media/evangelion.51c8e8ca.pdf"
+  },
+  {
+    "revision": "20f88a12ad2e6c2fbfb622ba61317cc9",
+    "url": "/dima-stories/static/media/GANTZ.20f88a12.docx"
+  },
+  {
+    "revision": "21bc91300d3e1b65ae961c244b3b6489",
+    "url": "/dima-stories/static/media/GANTZ.21bc9130.pdf"
+  },
+  {
+    "revision": "c7ba5dccf6d4ed3c372a8862440c54d5",
+    "url": "/dima-stories/static/media/Darth Vader Rebirth.c7ba5dcc.docx"
+  },
+  {
+    "revision": "3c8124184d12de79606c48cd208e1a49",
+    "url": "/dima-stories/static/media/Darth Vader Rebirth.3c812418.pdf"
+  },
+  {
+    "revision": "6cce490cfbc1789b0c18805e074198a5",
+    "url": "/dima-stories/static/media/Darth Vader- Hero of Naboo.6cce490c.docx"
+  },
+  {
+    "revision": "0e327244933c3ce318f96d45bf561900",
+    "url": "/dima-stories/static/media/Darth Vader- Hero of Naboo.0e327244.pdf"
+  },
+  {
+    "revision": "7d8159b49a223deb0e27a157184adc01",
+    "url": "/dima-stories/static/media/A Siths Second Chance.7d8159b4.docx"
+  },
+  {
+    "revision": "18d9a08ca0aec313a4c7353811637525",
+    "url": "/dima-stories/static/media/A Siths Second Chance.18d9a08c.pdf"
+  },
+  {
+    "revision": "8eb8be842bf03cfdf39bb73ee3912971",
+    "url": "/dima-stories/static/media/Mace Windus alternate self.8eb8be84.docx"
+  },
+  {
+    "revision": "3733de794fcc1fa8933554033174767c",
+    "url": "/dima-stories/static/media/Mace Windus alternate self.3733de79.pdf"
+  },
+  {
+    "revision": "442be4dc7915337de1475399e71d588b",
+    "url": "/dima-stories/static/media/Operation Preventing Vader.442be4dc.docx"
+  },
+  {
+    "revision": "7fd1ce85f8b7774b5438c69fbbc4dc41",
+    "url": "/dima-stories/static/media/Operation Preventing Vader.7fd1ce85.pdf"
+  },
+  {
+    "revision": "540b581dc129e34e4506d3768b1bcf77",
+    "url": "/dima-stories/static/media/Innocent.540b581d.docx"
+  },
+  {
+    "revision": "2c8e54183ba100272cce81eca7f9d233",
+    "url": "/dima-stories/static/media/Innocent.2c8e5418.pdf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/dima-stories/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/dima-stories/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/dima-stories/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/dima-stories/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/dima-stories/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/dima-stories/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/dima-stories/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/dima-stories/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/dima-stories/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/dima-stories/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/dima-stories/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/dima-stories/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/dima-stories/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/dima-stories/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/dima-stories/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a71b1a6a6e2bd3cc75fc",
+    "url": "/dima-stories/static/css/1.74083ecb.chunk.css"
+  },
+  {
+    "revision": "2cce50e9a84a3fff5aee3ab35b463080",
+    "url": "/dima-stories/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dima-stories/precache-manifest.c943db1437c250d85716f86e3c910989.js"
+  "/dima-stories/precache-manifest.6596ea75e4f8e092a5eeb1f8cf04a5b4.js"
 );
 
 workbox.clientsClaim();
